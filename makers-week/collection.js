e00519132744c5fd90f71a8b0b8b30633b4d2fa4
Tem Akinduro <takinduro@Tems-MacBook-Pro.local>
2023-10-19 16:44:07 -0400

    changes to scroll and active

--- makers-week/collection.js
+++ makers-week/collection.js
@@ -1,5 +1,6 @@
 let collectionSections = document.querySelectorAll('div.collection-story');
 let lists = document.querySelectorAll('.collection-list');
+const firstOffset = collectionSections[0].offsetTop;
 activeLink = (li) => {
     lists.forEach((item) => item.classList.remove('active'));
     li.classList.add('active');
@@ -8,16 +9,30 @@ lists.forEach((item) => {
     item.addEventListener('click', () => activeLink(this));
 });
 
-window.onscroll = () => {
+setActiveLink = () => {
     collectionSections.forEach((sec) => {
-        let top = window.scrollY;
+        let top = window.scrollY + 40;
         let offset = sec.offsetTop;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
+
+        console.log('top', top);
+
+        if (offset === firstOffset) {
+            activeLink(lists[0]);
+        }
 
         if (top >= offset && top < offset + height) {
             const target = document.querySelector(`[href='#${id}']`).parentElement;
             activeLink(target);
         }
     });
+};
+
+window.onload = () => {
+    setActiveLink();
+}
+
+window.onscroll = () => {
+    setActiveLink();
 }
